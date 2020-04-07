e921279b6e061584ca016a047f69a15ec2b44b71
Noah LeClaire <noah@Interapts-MacBook-Pro-5.local>
2020-04-06 22:16:56 -0400

    fixed PlayerCard

--- src/components/PlayerCard.vue
+++ src/components/PlayerCard.vue
@@ -1,6 +1,31 @@
 <template>
   <v-card>
-    {{ user.username}}, {{ user.party }}
+    <div>
+        <v-list-item>
+            <v-list-item-content>
+                <v-list-item-title class="headline mb-1"> {{ user.username}} </v-list-item-title>
+                <v-list-item-title class="headline mb-1">{{ user.party }}</v-list-item-title>
+          <v-list-item-subtitle>{{isHitler}}</v-list-item-subtitle>
+                <!-- <v-card v-if="user.party==='Fascist'" class="fascist"></v-card>
+                <v-card v-else class="policy liberal"></v-card> -->
+            </v-list-item-content> 
+            <v-list-item-avatar
+                tile
+                size="80"
+                v-if="user.party==='Fascist'" class="fascist"
+                ></v-list-item-avatar>
+            <v-list-item-avatar
+                tile
+                size="80"
+                v-else class="liberal"
+                ></v-list-item-avatar>
+                
+        </v-list-item>
+        <v-card-actions>
+            <v-btn>Vote Ja!</v-btn>
+            <v-btn>Vote Nein!</v-btn>
+        </v-card-actions>
+    </div>
   </v-card>
 </template>
 
@@ -9,9 +34,35 @@ import { mapState } from 'vuex'
 export default {
   name: 'player-card',
   computed: {
-    ...mapState({ user: 'user'})
+    ...mapState({ user: 'user'}),
+    isHitler() {
+        if (this.user.isHitler === true) {
+            return 'You are Hitler'
+        } else {
+            return 'You are not Hitler'
+        }
+    }
   }
 }
 </script>
 <style scoped>
+.policy {
+  max-width: 75px;
+  height: 125px;
+  margin-right: 10px;
+}
+.fascist{
+  background: red;
+}
+.liberal{
+  background: blue;
+}
+.player-hand {
+  padding: 10px;
+  display: flex;
+  
+}
+.button {
+    flex: wrap;
+}
 </style>
